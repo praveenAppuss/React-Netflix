86665b8f3bb23c791f806724b596877e7790a37c
Praveen MP <praveenmp490@gmail.com>
2026-05-12 20:08:54 +0530

    api youtube video done through the player.jsx

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
 import { Routes,Route } from 'react-router-dom'
+import Player from './Pages/Player/Player'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path='/player/:id' element={<Player />}/>
       </Routes>
       
       
--- Netflix-clone/src/Components/TitleCards/TitleCards.jsx
+++ Netflix-clone/src/Components/TitleCards/TitleCards.jsx
@@ -1,11 +1,24 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 
 
 
 const TitleCards = ({title,category}) => {
+
+  const[apiData,setApiData]=useState([]);
   const cardsRef =useRef();
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTEyNTdiMjczZDMyNWNhYzZiZmIyYjY5MzM1ODQwYiIsIm5iZiI6MTc3ODU2NzQwMi44ODk5OTk5LCJzdWIiOiI2YTAyYzhlYWE4NTg0ODg0MzI0ZDcxNjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZcHUyAU9sxipS2zkYX4cabCcBo1W2ZV-QQ2ttnLUQTk'
+  }
+};
+
+
 
 const handleWheel =(event)=>{
   event.preventDefault();
@@ -14,6 +27,12 @@ const handleWheel =(event)=>{
 }
 
 useEffect(()=>{
+
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel',handleWheel)
 
 },[])
@@ -21,11 +40,11 @@ useEffect(()=>{
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card,index)=>{
-          return <div className='card' key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card,index)=>{
+          return <Link to={`/player/${card.id}`} className='card' key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500/`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
         })}
       </div>
     </div>
