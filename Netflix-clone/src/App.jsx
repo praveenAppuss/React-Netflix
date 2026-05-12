29bd0af221193f7f03280fd837a04d16934ed5f7
Praveen MP <praveenmp490@gmail.com>
2026-05-12 11:56:23 +0530

    Login page design implemented with perfect title using useState

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+      
       
     </div>
   )
