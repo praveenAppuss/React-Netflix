e29e213551f7bc8c661351ca7681f8a9fe5371c9
Praveen MP <praveenmp490@gmail.com>
2026-05-13 20:52:49 +0530

    proper error message added and spinner gif added

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -5,6 +5,9 @@ import { Routes,Route, useNavigate } from 'react-router-dom'
 import Player from './Pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
+
+
 
 const App = () => {
   const navigate =useNavigate();
@@ -23,6 +26,7 @@ const App = () => {
   },[])
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
--- Netflix-clone/src/firebase.js
+++ Netflix-clone/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth"
 import {addDoc, collection, getFirestore} from "firebase/firestore"
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDy92H6-0nxHKAuUaE4TASUtoKAi7YsuOg",
@@ -28,7 +29,7 @@ const signup =async (name,email,password)=>{
         })
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "))
         
     }
 
@@ -39,7 +40,7 @@ const login = async (email,password)=>{
         await signInWithEmailAndPassword(auth,email,password)
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "))
     }
 
 }
