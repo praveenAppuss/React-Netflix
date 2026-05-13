460d0924c1d2511ad1d41d6197f4823a61f0f4d9
Praveen MP <praveenmp490@gmail.com>
2026-05-13 20:12:44 +0530

    feat user Authentication implemented using firebase.set signup login logout fuctions

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, useNavigate } from 'react-router-dom'
 import Player from './Pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
 const App = () => {
+  const navigate =useNavigate();
+  useEffect(()=>{
+    onAuthStateChanged(auth,async (user)=>{
+      if(user){
+        console.log("Logged In")
+        navigate('/');
+      }else{
+        console.log("Logged Out")
+        navigate('/login');
+      }
+
+    })
+
+  },[])
   return (
     <div>
       <Routes>
---
+++ Netflix-clone/src/firebase.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import {addDoc, collection, getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy92H6-0nxHKAuUaE4TASUtoKAi7YsuOg",
+  authDomain: "netflix-clone-618f6.firebaseapp.com",
+  projectId: "netflix-clone-618f6",
+  storageBucket: "netflix-clone-618f6.firebasestorage.app",
+  messagingSenderId: "569445021275",
+  appId: "1:569445021275:web:d57281a999bb807f64317e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup =async (name,email,password)=>{
+    try {
+        const res=await createUserWithEmailAndPassword(auth,email,password);
+        const user =res.user;
+        await addDoc(collection(db,"user"), {
+            uid:user.uid,
+            name,
+            authProvider:"local",
+            email,
+        })
+    } catch (error) {
+        console.log(error);
+        alert(error);
+        
+    }
+
+}
+
+const login = async (email,password)=>{
+    try {
+        await signInWithEmailAndPassword(auth,email,password)
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+
+}
+
+const logout =()=>{
+    signOut(auth);
+}
+
+export {auth,db,login,signup,logout};
